15dce7340b65a6e781061e9195ef1c401dd4de53
RaduFilip <filip16radu.m@gmail.com>
2022-04-03 22:11:39 +0300

    Add correct style.css file

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const container = document.querySelector('div');
+canvas(16)
 
-
-for(let j = 0; j < 16; j++ ){
+function canvas(dimension){
+/* Creates 16 rows */
+for(let j = 0; j < dimension; j++ ){
     const row = document.createElement('div');
-    row.style.cssText = "display: flex; flex-wrap: no-wrap";
-    for(let i = 0; i < 16; i++ ){
+    row.style.cssText = "display: flex; flex-wrap: no-wrap;  ";
+    /* Each row has 16 squares */
+    for(let i = 0; i < dimension; i++ ){
         const square = document.createElement('div');
-        square.style.cssText = "display: inline-block; height: 100px; width: 100px; background-color: red; border: black; border-style: solid; border-width: 10px";
+        square.style.cssText = "height:20px; aspect-ratio: 1/1;flex-grow:0; background-color: red; border: white; border-style: solid; border-width: 2%";
         row.appendChild(square);
-    }
+        square.addEventListener('mouseover', function(e){
+             e.target.style.background = 'blue';
+    });
+    /* Each row is then inserted into the main div container */
     container.appendChild(row)
+}
+}
 }
 
 
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', function(){
+    let dimension = parseInt(prompt("Choose new canvas dimension"));
+    container.innerHTML = "";
+
+    canvas(dimension);
+});
