22d9ba958b12de7b177f0e1f620ebfebdf8d97bf
RaduFilip <filip16radu.m@gmail.com>
2022-04-03 22:46:50 +0300

    Change main container so that it maintains size and add random color change for tiles hovering

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 const container = document.querySelector('div');
 canvas(16)
 
+
+
+
+
 function canvas(dimension){
 /* Creates 16 rows */
 for(let j = 0; j < dimension; j++ ){
     const row = document.createElement('div');
-    row.style.cssText = "display: flex; flex-wrap: no-wrap;  ";
+    row.style.cssText = "display: flex; flex-wrap: no-wrap;";
     /* Each row has 16 squares */
     for(let i = 0; i < dimension; i++ ){
         const square = document.createElement('div');
-        square.style.cssText = "height:20px; aspect-ratio: 1/1;flex-grow:0; background-color: red; border: white; border-style: solid; border-width: 2%";
+        square.style.cssText = "height:auto; aspect-ratio: 1/1;flex-grow:1; background-color: red; border: white; border-style: solid; border-width: 2%";
         row.appendChild(square);
         square.addEventListener('mouseover', function(e){
-             e.target.style.background = 'blue';
+            let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+             e.target.style.background = `#${randomColor}`;
     });
     /* Each row is then inserted into the main div container */
     container.appendChild(row)
 }
 }
 }
-
 
 const btn = document.querySelector('.btn');
 btn.addEventListener('click', function(){
